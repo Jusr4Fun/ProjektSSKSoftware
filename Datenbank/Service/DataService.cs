e4889077ca324a16c580bafe40392b2b26872317
Jusr4Fun <dominikdominik@gmx.net>
2023-03-30 15:19:19 +0200

    Code Bereinigung

--- Datenbank/Service/DataService.cs
+++ Datenbank/Service/DataService.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿using System.Linq;
 using System.Collections.ObjectModel;
-using Datenbank;
 using Datenbank.Model;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.CodeDom;
 
 namespace Datenbank.Service;
 
